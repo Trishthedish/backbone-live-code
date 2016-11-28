68435375d3b251d888df92fa83c4eafc2aab52e5
Trish Griego <pmgriego@gmail.com>
2016-11-28 14:50:37 -0800

    able to add task succesfully

--- src/app/views/task_list_view.js
+++ src/app/views/task_list_view.js
@@ -1,5 +1,8 @@
 // task_list_views.js
 //  we need to import our new files where they're need
+
+// TaskListView will be responsible for handling these events. In fact, TaskListView is the only thing we'll need to modify for this entire lecture!
+
 import $ from 'jquery';
 
 import Backbone from 'backbone';
@@ -30,9 +33,14 @@ var TaskListView = Backbone.View.extend({
       });
       this.cardList.push(card);
     }, this); // bind `this` so it's available inside forEach
-  },
+
+    this.input = {
+     title: this.$('.new-task input[name="title"]'),
+     description: this.$('.new-task input[name="description"]')
+    };
+  }, // end of initialize function
   render: function() {
-// we loop through the list of TaskViews, render each one and append the result to our DOM object.
+    // we loop through the list of TaskViews, render each one and append the result to our DOM object.
 
     // Make sure the list in the DOM is empty
     // before we start appending items
@@ -49,6 +57,53 @@ var TaskListView = Backbone.View.extend({
     }, this);
 
     return this; // enable chained calls
+  },
+
+    // Now that our callback is being called, we need to handle the event. We know from our experience with jQuery that we need to do the following:
+  events: {
+    //has action of submitting
+    'submit .new-task': 'createTask',
+    // "event css-selector" setting it to clear input
+    'click .clear-button': 'clearInput'
+  },
+    createTask: function(event) {
+      // Normally a form submission will refresh the page.
+  // Suppress that behavior.
+      event.preventDefault();
+      console.log('createTask called');
+      // Get the input data from the form and turn it into a task
+      var task = this.getInput();
+
+      // Add the new task to our list of tasks
+      this.taskData.push(task);
+      // Create a card for the new task, and add it to our card list
+    var card = new TaskView({
+      task: task,
+      template: this.taskTemplate
+    });
+    this.cardList.push(card);
+
+    // Re-render the whole list, now including the new card
+    this.render();
+
+    // Clear the input form so the user can add another task
+    this.clearInput();
+
+
+  },
+  // Build a task from the data entered in the .new-task form
+    getInput: function() {
+      var task = {
+        title: this.input.title.val(),
+        description: this.input.description.val()
+      };
+      return task;
+    },
+
+    clearInput: function(event) {
+      console.log("clearInput called friend!");
+      this.input.title.val('');
+      this.input.description.val('');
   }
 });
 
